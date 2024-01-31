4d2ad147b322d0d9212bff1fa6369d0dcb92b023
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-01-31 16:39:00 -0500

    firth send to github page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/website/website.module').then(m => m.WebsiteModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home/inicio',
+    pathMatch: 'full'
+  }
+];
